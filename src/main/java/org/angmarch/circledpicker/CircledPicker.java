49520050d08703e0bd78478dd497036df77a5ae6
Angelo Marchesin <angelo.marchesin@xing.com>
2015-08-03 01:08:19 +0200

    Remove warnings + fix for thickness conflict

--- src/main/java/org/angmarch/circledpicker/CircledPicker.java
+++ src/main/java/org/angmarch/circledpicker/CircledPicker.java
@@ -9,6 +9,7 @@ import android.animation.Animator.AnimatorListener;
 import android.animation.ValueAnimator;
 import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -18,16 +19,33 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.animation.DecelerateInterpolator;
 
+@SuppressWarnings("unused")
 public class CircledPicker extends View {
-    static public boolean isAPI10 = Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB;
-    public static final String TAG = "CIRCLED PICKER";
+
+    private static final boolean IS_API_10 = Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB;
     private static final int VALUE_THRESHOLD = 270;
+    private static final int TURN = 360;
+    private static final int HALF = 180;
+    private static final int TURN_THRESHOLD = 300;
+    private static final int DURATION = 200;
+    private static final int FULL_OPACITY = 255;
+    private static final int ACUTE = 60;
+    private static final int THREE_QUARTER = 270;
+    private static final float PERCENT = 100f;
+    private static final float ALMOST_TURN = 359.99f;
+    private static final float RADIUS_MULTIPLIER = .3f;
+    private static final float REMAINDER = 0.01f;
+    private static final String HOURS = "hours";
+    private static final String MINUTES = "minutes";
+    private static final String NUMERIC = "numeric";
+
     private ValueAnimator mAngleAnimator;
     private RectF mArcRect;
     private RectF mArcInnerRect;
@@ -57,7 +75,7 @@ public class CircledPicker extends View {
     private int mSubLineColor;
     private int mTextColor;
 
-    public static enum PickerMode {
+    public enum PickerMode {
         HOURS_AND_SECONDS,
         TIME_OF_DAY,
         PERCENT,
@@ -79,15 +97,15 @@ public class CircledPicker extends View {
     }
 
     public void animateToValue(float value) {
-        animateChange((value * 360) / mMaxValue);
+        animateChange((value * TURN) / mMaxValue);
     }
 
-    public float getAngle(Point target, Point origin) {
-        float angle = (float) Math.toDegrees(Math.atan2(target.x - origin.x, target.y - origin.y)) + 180;
-        if(angle < 0) {
-            angle += 360;
+    public static float getAngle(Point target, Point origin) {
+        float angle = (float) Math.toDegrees(Math.atan2(target.x - origin.x, target.y - origin.y)) + HALF;
+        if (angle < 0) {
+            angle += TURN;
         }
-        return 360 - angle;
+        return TURN - angle;
     }
 
     public int getMultiply(float value) {
@@ -98,10 +116,7 @@ public class CircledPicker extends View {
         return mThickness;
     }
 
-    public float getValue() {
-        return mCurrentValue;
-    }
-
+    @Override
     public void onDraw(Canvas canvas) {
         setCirclesBoundingBoxes();
         drawBackground(canvas);
@@ -113,47 +128,39 @@ public class CircledPicker extends View {
     private void updateCirle(float angle) {
         mCurrentSweep = angle;
 
-        if(mCurrentSweep - mLastAngle < -VALUE_THRESHOLD
-                && !mIsFilled
-                && !mIsEmpty) {
+        if (mCurrentSweep - mLastAngle < -VALUE_THRESHOLD && !mIsFilled && !mIsEmpty) {
             mIsFilled = true;
-        } else if(mCurrentSweep - mLastAngle > VALUE_THRESHOLD
-                && !mIsEmpty
-                && !mIsFilled) {
+        } else if (mCurrentSweep - mLastAngle > VALUE_THRESHOLD && !mIsEmpty && !mIsFilled) {
             mIsEmpty = true;
         }
 
-        if(mCurrentSweep < 360
-                && mCurrentSweep > 300
-                && mIsFilled) {
+        if (mCurrentSweep < TURN && mCurrentSweep > TURN_THRESHOLD && mIsFilled) {
             mIsFilled = false;
-        } else if(mCurrentSweep < 60
-                && mCurrentSweep > 0
-                && mIsEmpty) {
+        } else if (mCurrentSweep < ACUTE && mCurrentSweep > 0 && mIsEmpty) {
             mIsEmpty = false;
         }
 
-        if(mIsFilled) {
-            mCurrentSweep = 359.99f;
-        } else if(mIsEmpty) {
+        if (mIsFilled) {
+            mCurrentSweep = ALMOST_TURN;
+        } else if (mIsEmpty) {
             mCurrentSweep = 0;
         } else {
             mLastAngle = mCurrentSweep;
         }
 
-        mCurrentValue = getMultiply(((mCurrentSweep + 0.01f) * mMaxValue) / 360);
+        mCurrentValue = getMultiply(((mCurrentSweep + REMAINDER) * mMaxValue) / TURN);
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
+    public boolean onTouchEvent(@NonNull MotionEvent event) {
         float angle = getAngle(new Point((int) event.getX(), (int) event.getY()),
                 new Point(mMidX, mMidY));
-        if(isClickable()) {
+        if (isClickable()) {
             switch (event.getAction()) {
                 case MotionEvent.ACTION_MOVE:
-                    if(Math.abs(mDownX - event.getX()) > mTouchSlop &&
+                    if (Math.abs(mDownX - event.getX()) > mTouchSlop &&
                             (mAngleAnimator != null && !mAngleAnimator.isRunning())) {
-                        if (!isAPI10) {
+                        if (!IS_API_10) {
                             updateCirle(angle);
                             postInvalidate();
                         }
@@ -165,8 +172,8 @@ public class CircledPicker extends View {
                     break;
                 case MotionEvent.ACTION_UP:
                 case MotionEvent.ACTION_CANCEL:
-                    if(Math.abs(mDownX - event.getX()) < mTouchSlop) {
-                        if (!isAPI10) {
+                    if (Math.abs(mDownX - event.getX()) < mTouchSlop) {
+                        if (!IS_API_10) {
                             animateChange(angle);
                             mIsFilled = mIsEmpty = false;
                         }
@@ -178,32 +185,32 @@ public class CircledPicker extends View {
     }
 
     public void setThickness(int thickness) {
-        this.mThickness = thickness;
+        mThickness = thickness;
         postInvalidate();
     }
 
     public void setValue(float value) {
         mCurrentValue = value;
-        mCurrentSweep = ((value * 360) / mMaxValue);
+        mCurrentSweep = ((value * TURN) / mMaxValue);
     }
 
-    private String addAZeroIfNeeds(int value) {
+    private static String addAZeroIfNeeds(int value) {
         return value > 9 ? value + "" : "0" + value;
     }
 
     private void animateChange(float finalAngle) {
-        if(!isAPI10) {
-            if(mAngleAnimator.isRunning()) {
+        if (!IS_API_10) {
+            if (mAngleAnimator.isRunning()) {
                 mAngleAnimator.end();
             }
             mAngleAnimator.setFloatValues(mLastAngle, finalAngle);
-            mAngleAnimator.setDuration(200);
+            mAngleAnimator.setDuration(DURATION);
             mAngleAnimator.setInterpolator(new DecelerateInterpolator());
             mAngleAnimator.addUpdateListener(new AnimatorUpdateListener() {
                 @Override
                 public void onAnimationUpdate(ValueAnimator animation) {
                     mCurrentSweep = (Float) animation.getAnimatedValue();
-                    mCurrentValue = getMultiply((mCurrentSweep * mMaxValue) / 360);
+                    mCurrentValue = getMultiply((mCurrentSweep * mMaxValue) / TURN);
                     postInvalidate();
                 }
             });
@@ -234,93 +241,96 @@ public class CircledPicker extends View {
         canvas.drawColor(Color.TRANSPARENT);
         mPaint.reset();
         mPaint.setAntiAlias(true);
-        mPaint.setAlpha(255);
+        mPaint.setAlpha(FULL_OPACITY);
     }
 
     private void drawCenteredText(Canvas canvas) {
         String centerLabel = "";
-        int textVerticalOffset;
 
         mPaint.reset();
-        mPaint.setAlpha(255);
+        mPaint.setAlpha(FULL_OPACITY);
         mPaint.setShader(null);
         mPaint.setTextSize(mTextSize);
         mPaint.setColor(mTextColor);
 
         switch (mPickerMode) {
             case TIME_OF_DAY:
-               centerLabel = getHoursString() + ":" + getMinutesString();
-               break;
+                centerLabel = getHoursString() + ':' + getMinutesString();
+                break;
             case HOURS_AND_SECONDS:
-               centerLabel = getHoursString() + "h " + getMinutesString() + "m";
-               break;
+                centerLabel = getHoursString() + "h " + getMinutesString() + 'm';
+                break;
             case PERCENT:
-               centerLabel = getPercentString();
-               break;
+                centerLabel = getPercentString();
+                break;
             case NUMERIC:
-               centerLabel = String.valueOf((int) getValue());
-               break;
+                centerLabel = String.valueOf((int) mCurrentValue);
+                break;
         }
 
         mPaint.getTextBounds(centerLabel, 0, centerLabel.length(), mTextBounds);
-        textVerticalOffset = mMidY + (mTextBounds.height() / 2);
-        canvas.drawText(centerLabel, mMidX - (mTextBounds.width() / 2), textVerticalOffset, mPaint);
+
+        float halfWidth = (float) mTextBounds.width() / 2;
+        float textVerticalOffset = mMidY + ((float) mTextBounds.height() / 2);
+        canvas.drawText(centerLabel, mMidX - halfWidth, textVerticalOffset, mPaint);
     }
 
     private String getPercentString() {
-        int percent = (int) ((getValue() / mMaxValue) * 100f);
-        return String.valueOf(percent) + "%";
+        int percent = (int) ((mCurrentValue / mMaxValue) * PERCENT);
+        return percent + "%";
     }
 
     private void drawPickerCircle(Canvas canvas) {
         mPath.reset();
-        mPath.arcTo(mArcRect, 270, mCurrentSweep);
-        mPath.arcTo(mArcInnerRect, 270 + mCurrentSweep, -mCurrentSweep);
+        mPath.arcTo(mArcRect, THREE_QUARTER, mCurrentSweep);
+        mPath.arcTo(mArcInnerRect, THREE_QUARTER + mCurrentSweep, -mCurrentSweep);
         mPath.close();
         mPaint.setColor(mLineColor);
         canvas.drawPath(mPath, mPaint);
     }
 
     private void drawShadow(Canvas canvas) {
-        mPath.arcTo(mShadowRect, 0, 359.9f);
-        mPath.arcTo(mShadowInnerRect, 359.9f, -359.9f);
+        mPath.arcTo(mShadowRect, 0, ALMOST_TURN);
+        mPath.arcTo(mShadowInnerRect, ALMOST_TURN, -ALMOST_TURN);
         mPath.close();
         mPaint.setColor(mSubLineColor);
         canvas.drawPath(mPath, mPaint);
     }
 
     private String getMinutesString() {
-        int hour = (int) (getValue() / 60);
-        int minutes = (int) (getValue() - (hour * 60));
+        int hour = (int) (mCurrentValue / ACUTE);
+        int minutes = (int) (mCurrentValue - (hour * ACUTE));
         return addAZeroIfNeeds(minutes);
     }
 
     private String getHoursString() {
-        int hour = (int) (getValue() / 60);
+        int hour = (int) (mCurrentValue / ACUTE);
         return addAZeroIfNeeds(hour);
     }
 
     private void init(Context context, AttributeSet attrs) {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CircledPicker);
         ViewConfiguration viewConfiguration = ViewConfiguration.get(context);
+        Resources resources = getResources();
         String pickerMode;
 
-        mLineColor = typedArray.getColor(R.styleable.CircledPicker_lineColor, R.color.defaultLineColor);
-        mSubLineColor = typedArray.getColor(R.styleable.CircledPicker_subLineColor, R.color.defaultSubLineColor);
-        mTextColor = typedArray.getColor(R.styleable.CircledPicker_textColor, R.color.defaultLineColor);
+        mLineColor = typedArray.getColor(R.styleable.CircledPicker_lineColor, resources.getColor(R.color.defaultLineColor));
+        mSubLineColor = typedArray.getColor(R.styleable.CircledPicker_subLineColor, resources.getColor(R.color
+                .defaultSubLineColor));
+        mTextColor = typedArray.getColor(R.styleable.CircledPicker_textColor, resources.getColor(R.color.defaultLineColor));
         mStep = typedArray.getInteger(R.styleable.CircledPicker_step, 1);
         mMaxValue = typedArray.getInteger(R.styleable.CircledPicker_maxValue, 100);
         mTextSize = typedArray.getDimensionPixelSize(R.styleable.CircledPicker_textSize, 0);
-        mThickness = typedArray.getDimensionPixelSize(R.styleable.CircledPicker_thickness,
+        mThickness = typedArray.getDimensionPixelSize(R.styleable.CircledPicker_outerThickness,
                 (int) MeasureUtils.convertDpToPixel(context, 5));
         mInnerThickness = mThickness - typedArray.getDimensionPixelSize(R.styleable.CircledPicker_innerThickness,
                 (int) MeasureUtils.convertDpToPixel(context, 1));
         pickerMode = typedArray.getString(R.styleable.CircledPicker_pickerMode);
 
-        if(pickerMode != null) {
-            mPickerMode = pickerMode.equalsIgnoreCase("hours") ? PickerMode.HOURS_AND_SECONDS :
-                    pickerMode.equalsIgnoreCase("minutes") ? PickerMode.TIME_OF_DAY :
-                            pickerMode.equalsIgnoreCase("numeric") ? PickerMode.NUMERIC :
+        if (pickerMode != null) {
+            mPickerMode = pickerMode.equalsIgnoreCase(HOURS) ? PickerMode.HOURS_AND_SECONDS :
+                    pickerMode.equalsIgnoreCase(MINUTES) ? PickerMode.TIME_OF_DAY :
+                            pickerMode.equalsIgnoreCase(NUMERIC) ? PickerMode.NUMERIC :
                                     PickerMode.PERCENT;
         } else {
             mPickerMode = PickerMode.NUMERIC;
@@ -338,6 +348,7 @@ public class CircledPicker extends View {
         mPath = new Path();
 
         typedArray.recycle();
+
         setClickable(true);
     }
 
@@ -360,14 +371,14 @@ public class CircledPicker extends View {
         mMidX = MeasureSpec.getSize(widthMeasureSpec) / 2;
         mMidY = MeasureSpec.getSize(heightMeasureSpec) / 2;
 
-        if (mMidX < mMidY){
+        if (mMidX < mMidY) {
             mRadius = mMidX;
         } else {
             mRadius = mMidY;
         }
 
-        if(mTextSize == 0) {
-            mTextSize = mRadius * .3f;
+        if (mTextSize == 0) {
+            mTextSize = mRadius * RADIUS_MULTIPLIER;
         }
         mInnerRadius = mRadius - mThickness;
         mArcRect.left = mMidX - mRadius;
